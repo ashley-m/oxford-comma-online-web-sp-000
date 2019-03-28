16ab778816ce35c51e283f61d4f28070ddd645b3
Ashley Miller <asm2fe@virginia.edu>
2019-03-28 02:09:10 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  temp=""
+  case array.size
+  when == 1
+    return array.join
+  when == 2
+    return array.join("and")
+  else
+    temp=array.pop
+    array<<"and"
+    array<<temp
+  end
+    
 end
